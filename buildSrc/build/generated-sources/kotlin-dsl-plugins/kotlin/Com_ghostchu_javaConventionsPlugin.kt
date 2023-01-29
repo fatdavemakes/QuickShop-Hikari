f56f1b126b9455117f368142c2a9d9c42984dfb2
Ghost_chu <ghostchu@111.com>
2023-01-29 23:55:31 +0800

    checkpoint

---
+++ buildSrc/build/generated-sources/kotlin-dsl-plugins/kotlin/Com_ghostchu_javaConventionsPlugin.kt
@@ -0,0 +1,18 @@
+/**
+ * Precompiled [com.ghostchu.java-conventions.gradle.kts][Com_ghostchu_java_conventions_gradle] script plugin.
+ *
+ * @see Com_ghostchu_java_conventions_gradle
+ */
+public
+class Com_ghostchu_javaConventionsPlugin : org.gradle.api.Plugin<org.gradle.api.Project> {
+    override fun apply(target: org.gradle.api.Project) {
+        try {
+            Class
+                .forName("Com_ghostchu_java_conventions_gradle")
+                .getDeclaredConstructor(org.gradle.api.Project::class.java, org.gradle.api.Project::class.java)
+                .newInstance(target, target)
+        } catch (e: java.lang.reflect.InvocationTargetException) {
+            throw e.targetException
+        }
+    }
+}
